c0b00c02a9832e2068b08d8e5e36556a2fb50345
MateHUN <matehunter123@gmail.com>
2024-07-22 05:54:58 +0200

    Createing menu

--- client/main.lua
+++ client/main.lua
@@ -1,2 +1,26 @@
 ESX = exports['es_extended']:getSharedObject()
 mCore = exports["mCore"]:getSharedObj()
+
+
+RegisterCommand("create", (function()
+     PetMenu = {}
+
+     local header = "Pet Controller"
+
+     PetMenu[#PetMenu + 1] = { title = header, description = "Here you can control all the pet stuff.", isMenuHeader = true }
+     PetMenu[#PetMenu + 1] = {
+          icon = "fas fa-circle-xmark",
+          title = " ",
+          description = "Close",
+          event = "mate-petcontroller:Close"
+     }
+
+     exports["ox_lib"]:registerContext({
+          id = "PetMenu",
+          title = "Pet Controller",
+          position = "top-right",
+          options = PetMenu
+     })
+
+     exports["ox_lib"]:showContext("PetMenu")
+end), false)
